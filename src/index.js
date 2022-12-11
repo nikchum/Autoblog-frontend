0ae064b76176c3f81bf64cef75888f0ca0250d6a
Nikolay Chumak <107250262+nikchum@users.noreply.github.com>
2022-12-11 14:11:48 +0200

    fix base path

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/Autoblog-frontend">
     <Provider store={store}>
       <App />
     </Provider>
