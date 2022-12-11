ea3ca4928bf7e7ecdabba301822fff87ebd03f6c
nikchum <nchumak94@gmail.com>
2022-12-11 13:47:18 +0200

    fix url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const instance = axios.create({
+//   baseURL: 'http://localhost:3002/api',
+// });
+
 const instance = axios.create({
-  baseURL: 'http://localhost:3002/api',
+  baseURL: 'https://autoblog-backend-production.up.railway.app/api',
 });
 
 instance.interceptors.request.use(config => {
